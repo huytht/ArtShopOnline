cac644460a3259d77f0ffb7d4853117632d8092f
Hienvth <80890913+Hienvth@users.noreply.github.com>
2022-04-15 18:38:01 +0700

    AG6MEN-28: Admin: change password

--- admin/src/pages/Settings.jsx
+++ admin/src/pages/Settings.jsx
@@ -1,16 +1,23 @@
-import React, { useEffect, useState } from "react";
-const Settings = () => {  
-  return (
-    <div className="col-12">
-      <div className="card">
-        <div className="card__body">
-         
+import React from 'react';
+import ChangePass from '../components/password/ChangePass';
 
-         
+
+const Settings = () => {
+    return (
+        <div>
+            <h2 className="page-header">
+               Setting
+            </h2>
+            <div className="col-12">
+                <div className="card">
+                    <div className="card__body">
+                        <ChangePass/>
+                    </div>
+                </div>
+
+            </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default Settings;
+export default Settings
