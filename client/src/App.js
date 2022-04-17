927f85f8dd1d1de7022aa697183ae0423b061466
Khanh Tran <khanh.trn2608@gmail.com>
2022-04-17 22:21:46 +0700

    AG6MEN-22:Pre View

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Header from "./components/header/header";
+import Product from "./pages/product/product";
+import Cart from "./pages/cart/cart";
+import Payments from "./pages/payments/payments";
 
 const App = () => {
     return (
         <BrowserRouter>
             <Header />
+            <Switch>
+                <Route exact path="/" component={Product} />
+                <Route path="/cart" component={Cart} />
+                <Route path="/payments" component={Payments} />
+            </Switch>
         </BrowserRouter>
     );
 }
--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -7,50 +7,22 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Badge from '@mui/material/Badge';
 import IconButton from '@mui/material/IconButton';
-import Avatar from "@mui/material/Avatar";
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import SearchIcon from "@mui/icons-material/Search";
 
-import LoginForm from "../login/login";
-import RegisterForm from "../register/register";
 import SearchForm from "../searchBar/searchBar";
 
 import { useDispatch, useSelector } from "react-redux";
-import { actSetProduct, actAutoLogin, actLogout, actSetCategory } from "../../actions/index";
+import { actSetProduct, actAutoLogin, actSetCategory } from "../../actions/index";
 
 const Header = () => {
     const dispatch = useDispatch();
     const carts = useSelector(state => state.Cart);
-    const Account = useSelector(state => state.Account);
-    const [redirect, setRedirect] = useState(false);
-    //login-form
-    const [openLogin, setOpenLogin] = useState(false);
-    const onOpenLogin = () => setOpenLogin(true);
-    const onCloseLogin = () => setOpenLogin(false);
-    //register-form
-    const [openRegister, setOpenRegister] = useState(false);
-    const onOpenRegister = () => setOpenRegister(true);
-    const onCloseRegister = () => setOpenRegister(false);
-    //search-from
+    const [redirect] = useState(false);
+
     const [openSearch, setOpenSearch] = useState(false);
     const onOpenSearch = () => setOpenSearch(true);
     const onCloseSearch = () => setOpenSearch(false);
-    // change-form-login vs register
-    const onChangeForm = () => {
-        setOpenLogin(!openLogin);
-        setOpenRegister(!openRegister);
-    }
-    //
-    const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
 
     useEffect(() => {
         fetch("https://tengu-nodejs.herokuapp.com/api/product/")
@@ -88,88 +60,17 @@ const Header = () => {
                 })
         }
     }, [dispatch])
-    const accountForm = () => {
-        return (
-            <Box display="flex">
-                <Box display="flex" onClick={handleClick} sx={{ cursor: "pointer" }}>
-                    <Avatar sx={{ width: 18, height: 18, p: 0.25, m: 0.25, bgcolor: "#F28705" }}>K</Avatar>
-                    <Box
-                        pt={0.5}
-                        pl={0.5}
-                        fontFamily="Tahoma"
-                        fontSize="14px"
-                    >
-                        {Account.account.firstName + " " + Account.account.lastName}
-                    </Box>
-                </Box>
-                <Menu
-                    anchorEl={anchorEl}
-                    open={open}
-                    onClose={handleClose}
-                    onClick={handleClose}
-                    PaperProps={{
-                        elevation: 0,
-                        sx: {
-                            overflow: 'visible',
-                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                            mt: 1.5,
-                            '&:before': {
-                                content: '""',
-                                display: 'block',
-                                position: 'absolute',
-                                top: 0,
-                                right: 14,
-                                width: 10,
-                                height: 10,
-                                bgcolor: 'background.paper',
-                                transform: 'translateY(-50%) rotate(45deg)',
-                                zIndex: 0,
-                            },
-                        },
-                    }}
-                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-                >
-                    <MenuItem>
-                        <Link to="/account" style={{
-                            textDecoration: "none",
-                            color: "black"
-                        }}>
-                            Tài khoản của tôi
-                        </Link>
-                    </MenuItem>
-                    <MenuItem>
-                        Đơn mua
-                    </MenuItem>
-                    <MenuItem onClick={() => {
-                        dispatch(actLogout());
-                        setRedirect(true);
-                        localStorage.setItem("access_token", "");
-                    }}>
-                        Đăng xuất
-                    </MenuItem>
-                </Menu>
-            </Box>
-        );
-    }
-    const loginAndRegisterForm = () => {
-        return (
-            <Box display="flex">
-                <Box className="header__top__text" onClick={onOpenLogin}>Đăng nhập</Box>
-                <Box className="header__top__text" onClick={onOpenRegister}>Đăng ký</Box>
-            </Box>
-        );
-    }
     return (
         <div className="header">
-            {redirect ? <Redirect to="/"/> : ""}
-            <LoginForm openLogin={openLogin} onCloseLogin={onCloseLogin} onChangeForm={onChangeForm} setRedirect={setRedirect}/>
-            <RegisterForm openRegister={openRegister} onCloseRegister={onCloseRegister} onChangeForm={onChangeForm} setRedirect={setRedirect}/>
+            {redirect ? <Redirect to="/" /> : ""}
             <SearchForm openSearch={openSearch} onCloseSearch={onCloseSearch} />
             <Box bgcolor="lightgray">
                 <Container maxWidth="md" className="header__top__container">
                     <p className="header__top__phone">Hot Line: 0123456789</p>
-                    {Account.isLogin ? accountForm() : loginAndRegisterForm()}
+                    <Box display="flex">
+                        <Box className="header__top__text">Đăng nhập</Box>
+                        <Box className="header__top__text">Đăng ký</Box>
+                    </Box>
                 </Container>
             </Box>
             <Box display="flex">
@@ -180,7 +81,7 @@ const Header = () => {
                 <Container maxWidth="md" className="header__bot__container">
                     <Box display="flex" width="60%" justifyContent="space-between" mt={-1}>
                         <Link className="header__bot__text">HOME</Link>
-                        <Link className="header__bot__text">PRODUCT</Link>
+                        <Link to="/" className="header__bot__text">PRODUCT</Link>
                         <Link className="header__bot__text">ABOUT US</Link>
                         <Link className="header__bot__text">CONTACT</Link>
                         <Link className="header__bot__text hot__sale">HOTSALE</Link>
@@ -189,7 +90,7 @@ const Header = () => {
                         <IconButton onClick={onOpenSearch}>
                             <SearchIcon />
                         </IconButton>
-                        <Link>
+                        <Link to="/cart">
                             <IconButton aria-label="cart">
                                 <Badge badgeContent={carts.allQuantity} color="info">
                                     <ShoppingCartIcon />
